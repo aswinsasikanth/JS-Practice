156aac04d56b177c078890060b3ca55a45910893
Aswin_Sasikanth <sasikanthh@gmail.com>
2023-09-27 11:56:04 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,50 @@
+// * Practice: Declare a variable city and assign it the name of your city.
+// var city = "Pamarru"
+// console.log(city)
+
+/* * Practice: Using the variable city, write an if-else condition that checks if the city is "Paris". 
+If it is, log "You're in the City of Lights!", otherwise log "Welcome to [your city]!". */
+
+// if (city == "Paris") {
+//     console.log("You're in City of Lights")
+// } else {
+//     console.log("Welcome to Pamarru")
+// }
+
+// var a = 10
+// var b = 6
+
+// a>b?console.log("a is big"): console.log("b is big")
+
+// const prompt = require ("prompt-sync")({sigint: true})
+
+// var userInput = parseFloat(prompt("Enter a number:"));
+
+// // Check if the userInput is a number
+// if (!isNaN(userInput)) {
+//   if (userInput > 0) {
+//     console.log("The number is positive.");
+//   } else if (userInput < 0) {
+//     console.log("The number is negative.");
+//   } else {
+//     console.log("The number is zero.");
+//   }
+// } else {
+//   console.log("Invalid input. Please enter a valid number.");
+// }
+
+// let number = prompt("Enter a number: ");
+
+// if (number > 0) {
+//   console.log("The number is positive.");
+// } else if (number < 0) {
+//   console.log("The number is negative.");
+// } else {
+//   console.log("The number is zero.");
+// } 
+
+ let i = 9 
+    for (let j = 1; j<= 10; j++){
+    console.log( i + " * " + j + " is " + i*j )
+    }
+    
